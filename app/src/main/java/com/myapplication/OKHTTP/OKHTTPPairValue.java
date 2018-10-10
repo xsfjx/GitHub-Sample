349486acd371176515143ae5adfd8f7347ddbb9d
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-10 09:33:21 +0330

    Add pairValue class

---
+++ app/src/main/java/com/myapplication/OKHTTP/OKHTTPPairValue.java
@@ -0,0 +1,19 @@
+package com.myapplication.OKHTTP;
+
+public class OKHTTPPairValue {
+
+    private String key, value;
+
+    public OKHTTPPairValue(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
