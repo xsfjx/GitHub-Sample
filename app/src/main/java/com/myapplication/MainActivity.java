fa09ae91dd5df7e12f2ffdbe805ec3d81b1ae296
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-10 11:13:17 +0330

    Implement API using with progress bar

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.myapplication.API.LastUpdateApi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Wait!!!");
+        progressDialog.show();
+
+        LastUpdateApi.getLastUpdate(response -> {
+            Log.i("Log", "run: ");
+            progressDialog.dismiss();
+        });
+
     }
 }
