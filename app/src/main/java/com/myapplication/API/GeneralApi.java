ef20cb5226014d86a8ad86dccdec3a8ca292ee08
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-10 09:58:22 +0330

    Add generalApi class

---
+++ app/src/main/java/com/myapplication/API/GeneralApi.java
@@ -0,0 +1,64 @@
+package com.myapplication.API;
+
+import com.myapplication.OKHTTP.OKHTTP;
+import com.myapplication.OKHTTP.OKHTTPPairValue;
+import com.myapplication.OKHTTP.OKHTTPRunnable;
+
+import java.util.ArrayList;
+
+public class GeneralApi {
+
+    static <T> void getItemList(final Class<T> cls, String url, OKHTTPRunnable runnable, ArrayList<OKHTTPPairValue> pairs) {
+
+        OKHTTP.postInBackground(url,
+                null,
+                runnable,
+                pairs,
+                ApiConfig.getHeaders());
+
+    }
+
+    static <T> void getItemList(final Class<T> cls, String url, OKHTTPRunnable runnable) {
+
+        OKHTTP.postInBackground(url,
+                null,
+                runnable,
+                new ArrayList<OKHTTPPairValue>(),
+                ApiConfig.getHeaders());
+
+
+        /*if (response.isSuccess()) {
+            try {
+                Type type = new ParameterizedType() {
+                    @Override
+                    public Type[] getActualTypeArguments() {
+                        return new Type[]{cls};
+                    }
+
+                    @Override
+                    public Type getOwnerType() {
+                        return ArrayList.class;
+                    }
+
+                    @Override
+                    public Type getRawType() {
+                        return null;
+                    }
+                };
+                return ModelParser.getGson().fromJson(response.getBody().getJSONArray(Tags.DATA).toString(), type);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return null;*/
+    }
+
+
+    static <T> void getItemList(String url, OKHTTPRunnable runnable) {
+        OKHTTP.getInBackground(url,
+                runnable,
+                ApiConfig.getHeaders());
+
+    }
+
+}
