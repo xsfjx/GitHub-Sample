3ad410d3ebd5951ff7440fcd848fc2df664c2016
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-16 09:13:38 +0330

    Add uncaughtExceptionHandler

---
+++ app/src/main/java/com/myapplication/MyUncaughtExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.myapplication;
+
+import android.util.Log;
+
+public class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        Log.i("Log", "uncaughtException: ");
+    }
+}
