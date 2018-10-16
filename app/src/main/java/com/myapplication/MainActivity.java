e6bf4bf2c4eb5862701d25f6d89393ec0379601d
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-16 10:04:18 +0330

    Implement design to code

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.myapplication.API.LastUpdateApi;
+import com.myapplication.DTO.MainDto;
+import com.myapplication.OKHTTP.OKHTTPResponse;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +23,21 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setMessage("Wait!!!");
         progressDialog.show();
 
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtLogin = findViewById(R.id.txtLogin);
+        TextView txtBio = findViewById(R.id.txtBio);
+        TextView txtLocation = findViewById(R.id.txtLocation);
+
         LastUpdateApi.getLastUpdate(response -> {
             Log.i("Log", "run: ");
             progressDialog.dismiss();
+            MainDto dto = OKHTTPResponse.getMainDto();
+            Picasso.get().load(dto.getAvatar_url()).into(imageView);
+            txtName.setText(dto.getName());
+            txtLogin.setText(dto.getLogin());
+            txtBio.setText(dto.getBio());
+            txtLocation.setText(dto.getLocation());
         });
 
     }
