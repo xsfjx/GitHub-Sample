0856c83e94255e13bdea606c6cd701d097dd53e6
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-16 09:21:32 +0330

    Add Dto

---
+++ app/src/main/java/com/myapplication/DTO/MainDto.java
@@ -0,0 +1,58 @@
+package com.myapplication.DTO;
+
+public class MainDto {
+
+    private String login;
+    private String avatar_url;
+    private String name;
+    private String location;
+    private String bio;
+
+    public MainDto() {
+        login = "";
+        avatar_url = "";
+        name = "";
+        location = "";
+        bio = "";
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
