d1583b45840fe57e777208475666c174cde6b670
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-17 09:53:15 +0330

    Add Constructor

--- app/src/main/java/com/githubsample/MyUncaughtExceptionHandler.java
+++ app/src/main/java/com/githubsample/MyUncaughtExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.githubsample;
 
+import android.content.Context;
 import android.util.Log;
 
 public class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
+    private Context context;
+
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
         Log.i("Log", "uncaughtException: ");
+        
+    }
+
+    public MyUncaughtExceptionHandler(Context context) {
+        this.context = context;
     }
 }
