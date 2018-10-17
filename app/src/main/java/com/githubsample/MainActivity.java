a99c8fe15e923fc609c915301ec2fabc9e6d7dc8
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-17 11:11:04 +0330

    Implement okhttp access

--- app/src/main/java/com/githubsample/MainActivity.java
+++ app/src/main/java/com/githubsample/MainActivity.java
@@ -1,44 +1,71 @@
 package com.githubsample;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.githubsample.helper.base.BaseActivity;
+import com.githubsample.helper.messages.MessageHelper;
 import com.githubsample.tools.api.LastUpdateApi;
 import com.githubsample.tools.dto.MainDto;
 import com.githubsample.tools.okhttp.OKHTTPResponse;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends BaseActivity {
+
+    private ImageView imageView;
+    private TextView txtName;
+    private TextView txtLogin;
+    private TextView txtLocation;
+    private TextView txtBio;
+    private MainDto mainDto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setMessage("Wait!!!");
-        progressDialog.show();
-
-        ImageView imageView = findViewById(R.id.imageView);
-        TextView txtName = findViewById(R.id.txtName);
-        TextView txtLogin = findViewById(R.id.txtLogin);
-        TextView txtBio = findViewById(R.id.txtBio);
-        TextView txtLocation = findViewById(R.id.txtLocation);
+        initActivity();
+        showWaitingDialog();
 
         LastUpdateApi.getLastUpdate(response -> {
             Log.i("Log", "run: ");
-            progressDialog.dismiss();
-            MainDto dto = OKHTTPResponse.getMainDto();
-            Picasso.get().load(dto.getAvatar_url()).into(imageView);
-            txtName.setText(dto.getName());
-            txtLogin.setText(dto.getLogin());
-            txtBio.setText(dto.getBio());
-            txtLocation.setText(dto.getLocation());
+            fillMainDto(response);
+            Picasso.get().load(mainDto.getAvatar_url()).into(imageView);
+            txtName.setText(mainDto.getName());
+            txtLogin.setText(mainDto.getLogin());
+            txtBio.setText(mainDto.getBio());
+            txtLocation.setText(mainDto.getLocation());
+            dismissWaitingDialog();
+            MessageHelper.showMessage(this, "Data is loaded!");
         });
 
+    }
+
+    private void initActivity() {
+        mainDto = new MainDto();
+        imageView = findViewById(R.id.imageView);
+        txtName = findViewById(R.id.txtName);
+        txtLogin = findViewById(R.id.txtLogin);
+        txtBio = findViewById(R.id.txtBio);
+        txtLocation = findViewById(R.id.txtLocation);
+    }
+
+    private MainDto fillMainDto(OKHTTPResponse response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response.getBody().toString());
+            mainDto.setLogin(jsonObject.getString("login"));
+            mainDto.setAvatar_url(jsonObject.getString("avatar_url"));
+            mainDto.setName(jsonObject.getString("name"));
+            mainDto.setLocation(jsonObject.getString("location"));
+            mainDto.setBio(jsonObject.getString("bio"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return mainDto;
     }
 }
