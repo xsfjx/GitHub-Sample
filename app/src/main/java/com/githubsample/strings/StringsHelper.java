9b543bf341c99bd8bc9ef653b701c0b4bb1cab57
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-17 09:25:22 +0330

    Add StringHelper

---
+++ app/src/main/java/com/githubsample/strings/StringsHelper.java
@@ -0,0 +1,46 @@
+package com.githubsample.strings;
+
+import com.githubsample.MyApplication;
+
+public class StringsHelper {
+
+    public static float parseToFloat(Object obj) {
+        return Float.parseFloat(obj.toString());
+    }
+
+    public static int roundTheFloat(Object obj) {
+        return Math.round(Float.parseFloat(obj.toString()));
+    }
+
+    public static String translateTimerProgress(int progress) {
+        String timerValue;
+        switch (progress) {
+            case 1:
+                timerValue = "45";
+                break;
+            case 2:
+                timerValue = "60";
+                break;
+            case 3:
+                timerValue = "90";
+                break;
+            case 4:
+                timerValue = "120";
+                break;
+            case 5:
+                timerValue = "150";
+                break;
+            default:
+                timerValue = "";
+                break;
+        }
+        timerValue += " ثانیه";
+        return timerValue;
+
+    }
+
+    public static String getString(int id) {
+        return MyApplication.context.getString(id);
+    }
+
+}
