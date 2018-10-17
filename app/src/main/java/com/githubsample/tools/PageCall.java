0966d29ff7bc1025debe3e64e8886740fdb7dc74
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-17 08:54:22 +0330

    Create a page call class

---
+++ app/src/main/java/com/githubsample/tools/PageCall.java
@@ -0,0 +1,25 @@
+package com.githubsample.tools;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.githubsample.MainActivity;
+
+public class PageCall {
+
+    public static void startSecondActivity(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+    public static void startSecondActivity(Context context , String name) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(Tags.F_NAME , name);
+        context.startActivity(intent);
+    }
+
+
+
+
+}
