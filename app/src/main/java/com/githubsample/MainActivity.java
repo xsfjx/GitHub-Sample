04ae694e01283da9307de9088da2d315d41799c0
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-24 09:13:03 +0330

    Implement using permissionHelper

--- app/src/main/java/com/githubsample/MainActivity.java
+++ app/src/main/java/com/githubsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.githubsample;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.githubsample.helper.base.BaseActivity;
 import com.githubsample.helper.messages.MessageHelper;
+import com.githubsample.helper.permission.PermissionHelper;
 import com.githubsample.tools.api.LastUpdateApi;
 import com.githubsample.tools.dto.MainDto;
 import com.githubsample.tools.okhttp.OKHTTPResponse;
@@ -16,6 +18,8 @@ import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import static com.githubsample.tools.Tags.MY_PERMISSIONS_REQUEST_READ_CONTACTS;
 
 public class MainActivity extends BaseActivity {
 
@@ -30,19 +34,39 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        PermissionHelper.getPermission(this);
         initActivity();
-        showWaitingDialog();
-        LastUpdateApi.getLastUpdate(response -> {
-            Log.i("Log", "run: ");
-            fillMainDto(response);
-            initImage();
-            dismissWaitingDialog();
-            MessageHelper.showMessage(this, "Data is loaded!");
-        });
-
+//        showWaitingDialog();
+//        LastUpdateApi.getLastUpdate(response -> {
+//            Log.i("Log", "run: ");
+//            fillMainDto(response);
+//            initImage();
+//            dismissWaitingDialog();
+//            MessageHelper.showMessage(this, "Data is loaded!");
+//        });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("Log", "onRequestPermissionsResult: ");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Log.i("Log", "onRequestPermissionsResult: ");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
     private void initActivity() {
         mainDto = new MainDto();
         imageView = findViewById(R.id.imageView);
