487342f2ef3f37ee1a70ff66dacb55223311d376
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-24 10:38:34 +0330

    Refactor name of class and method

--- app/src/main/java/com/githubsample/tools/api/github/GitHubApi.java
+++ app/src/main/java/com/githubsample/tools/api/github/GitHubApi.java
@@ -1,14 +1,15 @@
-package com.githubsample.tools.api;
+package com.githubsample.tools.api.github;
 
+import com.githubsample.tools.api.GeneralApi;
 import com.githubsample.tools.okhttp.OKHTTPRunnable;
 
-public class LastUpdateApi {
+public class GitHubApi {
 
     // ****************** URL *************************
     private static final String LAST_UPDATE_URL = "https://api.github.com/users/xsfjx";
 
     // ****************** API ********************
-    public static  void getLastUpdate(OKHTTPRunnable runnable){
+    public static  void getProfile(OKHTTPRunnable runnable){
         String url = LAST_UPDATE_URL;
         GeneralApi.getItemList(url,runnable);
     }
