07c4ddc72cd32cf77516fe4898b82c72eac0f50f
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-24 10:44:40 +0330

    Make view interface

--- app/src/main/java/com/githubsample/main/IMainView.java
+++ app/src/main/java/com/githubsample/main/IMainView.java
@@ -1,9 +1,19 @@
 package com.githubsample.main;
 
+import android.graphics.Bitmap;
+
+import com.githubsample.tools.dto.MainDto;
+
 public interface IMainView {
 
     void showMsg (String msg);
+
     void showProgress ();
+
     void closeProgress ();
+
+    void fillData(MainDto dto);
+
+    void setAvatarImageView(Bitmap bitmap);
 
 }
